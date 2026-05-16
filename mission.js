2140fb7db6dfecb4347733491bc775edf5b27988
circe condales <circec@byui.edu>
2026-05-15 23:33:02 -0400

    update

--- mission.js
+++ mission.js
@@ -1,20 +1,33 @@
 
 let selectElem = document.querySelector('select');
 let logo = document.querySelector('img');
+let pageContent = document.querySelector('.content');
 
 selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
     let current = selectElem.value;
-    if (current == 'dark') {
+    if (current === 'dark') {
         // code for changes to colors and logo
-        document.body.style.backgroundImage = 'ponder\images\byui-logo-white.png';
+        // Dark mode styles
+        document.body.style.backgroundColor = "#333";
+        document.body.style.color = "white";
+
         pageContent.style.backgroundColor = "#1a1a1a";
+        pageContent.style.color = "white";
+
+        logo.src = "images/byui-logo-white.png";
 
     } else {
         // code for changes to colors and logo
-        document.body.style.backgroundImage = 'ponder\images\byui-logo-white.png';
-        pageContent.style.backgroundColor = "#ffff";
+         // Light mode styles
+        document.body.style.backgroundColor = "#ddd";
+        document.body.style.color = "black";
+
+        pageContent.style.backgroundColor = "white";
+        pageContent.style.color = "black";
+
+        logo.src = "images/BYUIPHOTO.png";
 
     }
 }
