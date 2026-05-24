b4f24aba2c25d7183464de2200e248026646eba0
circe condales <circec@byui.edu>
2026-05-23 22:42:43 -0400

    update

--- modal.js
+++ modal.js
@@ -2,15 +2,24 @@
 const gallery = document.querySelector('.gallery');
 const modal = document.querySelector('dialog');
 const modalImage = modal.querySelector('img');
-const closeButton = modal.querySelector('.close-viewer');
+const closeButton = modal.querySelector('.close-viewer'); //popup
 
 // Event listener for opening the modal
 gallery.addEventListener('click', openModal);
 
 function openModal(e) {
-    
-// Code to show modal  - Use event parameter 'e'   
-    
+// Code to show modal  - Use event parameter 'e'
+    console.log(e.target);
+    const img = e.target;
+    const src = img.getAttribute('src');
+    const alt = img.getAttribute('alt');
+    const full = src.replace('sm', 'full');
+
+    modalImage.src = full;
+    modalImage.alt = alt;
+
+    modal.showModal();
+
 }
 // Close modal on button click
 closeButton.addEventListener('click', () => {
