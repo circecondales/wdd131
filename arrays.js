a42dd48681f456ae5b058421cb927705cfcefd5a
circe condales <circec@byui.edu>
2026-05-29 01:00:56 -0400

    update

---
+++ arrays.js
@@ -0,0 +1,63 @@
+
+const steps = ['one', 'two', 'three'];
+
+steps.forEach(function(item){
+    console.log(item);
+});
+
+steps.forEach(showSteps);
+
+function showSteps(steps){
+    console.log(steps);
+};
+
+//.map
+let myList = document.querySelector('#myList');
+
+const stepsHtml = steps.map(listTemplate);
+
+function listTemplate(item){
+    return `<li>${item}</li>`;
+}
+
+myList.innerHTML = stepsHtml.join('');
+
+let grades = ['A', 'B', 'C'];
+let point;
+
+let gpaPoints = grades.map(convert);
+
+function convert(grade) {
+    switch (grade){
+        case 'A':
+            points = 4;
+            break;
+        case 'B':
+            points = 3;
+            break;
+        case 'C':
+            points = 2;
+            break;
+        case 'D':
+            points = 1;
+            break;
+        case 'F':
+            points = 0;
+            break;
+        default:
+            alert('not a valid grade');
+    }
+    return points;
+}
+console.log(gpaPoints);
+
+//.reduce
+
+let totalPoints = gpaPoints.reduce(getTotal);
+function getTotal(total, item){
+    return total + item;
+}
+console.log(totalPoints);
+
+let gpaAverage = totalPoints / gpaPoints.length;
+console.log(gpaAverage);
