f3b11b7dea444c1dd9d2608f6087862f70ad9545
circe condales <circec@byui.edu>
2026-06-14 01:47:23 -0400

    update

---
+++ charactercard.js
@@ -0,0 +1,47 @@
+// Character Object
+const character = {
+    name: "Snortleblat",
+    class: "Swamp Beast Diplomat",
+    level: 1,
+    health: 100,
+    image: "images/snortleblat.png",
+
+    attacked: function () {
+        this.health -= 20;
+
+        if (this.health <= 0) {
+            this.health = 0;
+            alert(`${this.name} has died!`);
+        }
+
+        renderCharacter();
+    },
+
+    levelUp: function () {
+        this.level++;
+        renderCharacter();
+    }
+};
+
+// Display Character Information
+function renderCharacter() {
+    document.querySelector("#characterName").textContent = character.name;
+    document.querySelector("#characterClass").textContent = character.class;
+    document.querySelector("#characterLevel").textContent = character.level;
+    document.querySelector("#characterHealth").textContent = character.health;
+
+    document.querySelector("#characterImage").src = character.image;
+    document.querySelector("#characterImage").alt = character.name;
+}
+
+// Initial Render
+renderCharacter();
+
+// Event Listeners
+document.querySelector("#attackBtn").addEventListener("click", function () {
+    character.attacked();
+});
+
+document.querySelector("#levelBtn").addEventListener("click", function () {
+    character.levelUp();
+});
